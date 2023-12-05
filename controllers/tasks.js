2bc2e48c507504a18fa6c85ab8403cfa385476eb
justinbather <justinbather@hotmail.com>
2023-12-05 08:09:35 -0500

    adds getOneTask controller and route

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -10,6 +10,19 @@ const getAllTasks = async (req, res) => {
   }
 }
 
+const getOneTask = async (req, res) => {
+  try {
+    const taskId = req.params.id
+    const task = await Task.findById(taskId)
+    if (task) {
+      return res.status(200).json(task)
+    } else {
+      return res.status(404).json({ message: "Error: No task found with that Id" })
+    }
+  } catch (err) {
+    return res.status(500).json({ message: "Server Error fetching task", error: err.message })
+  }
+}
 
 const createTask = async (req, res) => {
   try {
@@ -60,4 +73,4 @@ const deleteTask = async (req, res) => {
   }
 }
 
-module.exports = { getAllTasks, createTask, updateTask, deleteTask }
+module.exports = { getAllTasks, createTask, updateTask, deleteTask, getOneTask }
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const { getAllTasks, createTask, updateTask, deleteTask } = require('../controllers/tasks')
+const { getAllTasks, createTask, updateTask, deleteTask, getOneTask } = require('../controllers/tasks')
 
 
 router = express.Router()
 
 
 router.get('/', getAllTasks)
+router.get('/:id', getOneTask)
 router.post('/', createTask)
 router.patch('/:id', updateTask)
 router.delete('/:id', deleteTask)
