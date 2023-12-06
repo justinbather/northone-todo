bee623d543259da33f5a373691f34ad7f3660042
justinbather <justinbather@hotmail.com>
2023-12-05 20:42:09 -0500

    adds tests

--- tests/taskList.test.js
+++ tests/taskList.test.js
@@ -4,6 +4,7 @@ const TaskList = require('../schema/taskListSchema')
 
 
 describe("Task List Controller Functions", () => {
+  let taskList;
   describe("Create Task List", () => {
     it("Should return 201 and task list created", async () => {
       const response = await request(app)
@@ -15,12 +16,22 @@ describe("Task List Controller Functions", () => {
         });
 
       expect(response.status).toBe(201)
+      taskList = response.body
     })
   })
   describe("Get Task List", () => {
     it("Should return 200 with task list", async () => {
       const response = await request(app)
         .get("/tasklists")
+
+      expect(response.status).toBe(200)
+    })
+  })
+
+  describe("Get One Task List", () => {
+    it("Should return 200 with task list", async () => {
+      const response = await request(app)
+        .get(`/tasklists/${taskList._id}`)
 
       expect(response.status).toBe(200)
     })
